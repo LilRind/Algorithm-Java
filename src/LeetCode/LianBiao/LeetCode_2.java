98d3f6ff7481ea186c98b61772b58d9be0e9cd37
LilRind <1337476737@qq.com>
2024-05-29 00:43:10 +0800

    LeetCode热题链表相关题目

--- src/LeetCode/LianBiao/LeetCode_2.java
+++ src/LeetCode/LianBiao/LeetCode_2.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 import static LeetCode.LianBiao.ListNode.ListNode.printNodes;
 
+// https://leetcode.cn/problems/add-two-numbers/  （1.递归 2.迭代）
+
+
 // 两数相加-链表
 /*
 输入：l1 = [2,4,3], l2 = [5,6,4]
@@ -42,39 +45,85 @@ public class LeetCode_2 {
 
     }
 
-    // 官方解法
+    // 第二次做，仍然不能通过不同链表长度的情况，再重写
+    /*
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // head指针节点记录链表首节点(指针不动)、tail指针节点是记录两数相加的新节点（最终也是最高位节点）
-        ListNode head = null, tail = null;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int n1 = l1 != null ? l1.val : 0; // 不为空则为l1.val，为空则为0
-            int n2 = l2 != null ? l2.val : 0; // 不为空则为l2.val，为空则为0
-            int sum = n1 + n2 + carry;
-            // 头部节点为空，刚开始没有头部节点
-            if (head == null) {
-                head = tail = new ListNode(sum % 10);
-            } else { // 头部指针已创建，创建两数相加的新节点
-                tail.next = new ListNode(sum % 10);
-                tail = tail.next;
-            }
-            carry = sum / 10; // 更新进位
-            // 如果不为空则继续遍历下一节点
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
+        ListNode dummyNode = new ListNode(); // 虚拟节点
+        ListNode h = dummyNode;
+        int cur = 0;
+
+        while (l1 != null && l2 != null){
+            ListNode temp = new ListNode((l1.val+l2.val+cur)%10);
+            h.next = temp;
+            h = h.next;
+            cur = (l1.val + l2.val+ cur)/10;
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        // 最高位还有进位，则创建新节点
-        if (carry > 0) {
-            tail.next = new ListNode(carry);
+        h.next = l1 == null ? l2 : l1;
+
+        return dummyNode.next;
+    }
+     */
+    // 再写，迭代
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(); // 虚拟节点
+        ListNode h = dummyNode;
+        int carry = 0; // 进位
+
+        // 四种情况，l1有节点，l2有节点，l1，l2都有节点，有进位
+        while (l1 != null || l2 != null || carry != 0){
+            if(l1 != null){ // l1有节点，累加到进位
+                carry += l1.val;
+            }
+            if(l2 != null){ // l2有节点，累加到进位
+                carry += l2.val;
+            }
+            // 创建节点，值为模除10，h.next指向temp，h指向h.next
+            ListNode temp = new ListNode(carry % 10);
+            h.next = temp;
+            h = h.next;
+            carry /= 10; // 计算进位
+            if(l1 != null) l1 = l1.next; // 如果l1有节点，指向下一位
+            if(l2 != null) l2 = l2.next; // 如果l2有节点，指向下一位
         }
-        return head;
+        return dummyNode.next; // 返回头节点
     }
 
-    // 第一次解，能通过同长度的两条列表（例如2、4、3-----5、6、4），但无法通过两条链表不一致的情况
+
+    // 第一次未解出，官方解法，模拟的做法相比之下有点复杂
+//    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        // head指针节点记录链表首节点(指针不动)、tail指针节点是记录两数相加的新节点（最终也是最高位节点）
+//        ListNode head = null, tail = null;
+//        int carry = 0;
+//        while (l1 != null || l2 != null) {
+//            int n1 = l1 != null ? l1.val : 0; // 不为空则为l1.val，为空则为0
+//            int n2 = l2 != null ? l2.val : 0; // 不为空则为l2.val，为空则为0
+//            int sum = n1 + n2 + carry;
+//            // 头部节点为空，刚开始没有头部节点
+//            if (head == null) {
+//                head = tail = new ListNode(sum % 10);
+//            } else { // 头部指针已创建，创建两数相加的新节点
+//                tail.next = new ListNode(sum % 10);
+//                tail = tail.next;
+//            }
+//            carry = sum / 10; // 更新进位
+//            // 如果不为空则继续遍历下一节点
+//            if (l1 != null) {
+//                l1 = l1.next;
+//            }
+//            if (l2 != null) {
+//                l2 = l2.next;
+//            }
+//        }
+//        // 最高位还有进位，则创建新节点
+//        if (carry > 0) {
+//            tail.next = new ListNode(carry);
+//        }
+//        return head;
+//    }
+
+    // 第一次解，我的方法，能通过同长度的两条列表（例如2、4、3-----5、6、4），但无法通过两条链表不一致的情况
 //    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 //        ListNode head = new ListNode();
 //        ListNode pos = head;
@@ -109,6 +158,7 @@ public class LeetCode_2 {
 //        }
 //        return head;
 //    }
+
 
 
 }
