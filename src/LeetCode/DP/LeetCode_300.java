3cb36941671bd4345488ed813551373cff451c61
LilRind <1337476737@qq.com>
2024-06-02 15:08:35 +0800

    LeetCode热题动态规划相关题目

--- src/LeetCode/DP/LeetCode_300.java
+++ src/LeetCode/DP/LeetCode_300.java
@@ -1,8 +1,21 @@
 package LeetCode.DP;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
+/*
+输入：nums = [10,9,2,5,3,7,101,18]
+输出：4
+解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+
+输入：nums = [0,1,0,3,2,3]
+输出：4
+
+输入：nums = [7,7,7,7,7,7,7]
+输出：1
+ */
 
 // LeetCode 300. 最长递增子序列
 // https://leetcode.cn/problems/longest-increasing-subsequence/description/
@@ -13,6 +26,12 @@ public class LeetCode_300 {
     static int n, nums[] = new int[N], f[] = new int[N];
 
     public static void main(String[] args) {
+        int[] num1 = {10, 9, 2, 5, 3, 7, 101, 18};
+
+        System.out.println(lengthOfLIS(num1));
+
+        // 之前的写法
+        /*
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         for (int i = 0; i < n; i++){
@@ -20,9 +39,70 @@ public class LeetCode_300 {
         }
         int result = lengthOfLIS(nums, n);
         System.out.println(result);  // 输出最长递增子序列的长度
+        */
     }
 
-    // 计算最长递增子序列的长度
+    // 第一次未解出。
+    public static int lengthOfLIS(int[] nums) {
+        // res代表最长递增子序列的长度。因为逻辑判断是当nums[j] < nums[i]才进行dp[]更新
+        int len = nums.length, res = 1;
+        // dp[i] 代表以 nums[i] 结尾的最长子序列长度。注意这里不是前i位的最长递增子序列长度
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){ // 当nums[j] < nums[i]才进行dp[]更新
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    res = Math.max(res, dp[i]);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    // K神：纯动态规划。O(N^2)，O(N)
+    /*
+    // Dynamic programming.
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+     */
+
+    // K神：动态规划 + 二分查找。O(NlogN)，O(N)
+    // 最快的解法之一
+    /*
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int res = 0;
+        for(int num : nums) {
+            int i = 0, j = res;
+            while(i < j) {
+                int m = (i + j) / 2;
+                if(tails[m] < num) i = m + 1;
+                else j = m;
+            }
+            tails[i] = num;
+            if(res == j) res++;
+        }
+        return res;
+    }
+
+     */
+
+
+    // 之前写的，计算最长递增子序列的长度
+    /*
     public static int lengthOfLIS(int[] nums, int n) {
         List<Integer> v = new ArrayList<>();  // 创建一个ArrayList用于存储递增子序列的元素
 
@@ -37,17 +117,7 @@ public class LeetCode_300 {
             }
             System.out.println("v：" + v);
         }
-        /*
-            输入：6
-            输入：0 1 0 3 2 3
-            v：[0]
-            v：[0, 1]
-            v：[0, 1]
-            v：[0, 1, 3]
-            v：[0, 1, 2]
-            v：[0, 1, 2, 3]
-            输出：4
-         */
+
 
         return v.size();  // 返回递增子序列的长度
     }
@@ -65,6 +135,8 @@ public class LeetCode_300 {
         }
         return left;  // 返回目标元素应该插入的位置
     }
+    */
+
 }
 /*
 import java.util.*;
