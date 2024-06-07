e68a9ba82e0412bb55ed2a6a032afdad1415888d
LilRind <1337476737@qq.com>
2024-06-07 10:03:23 +0800

    LeetCode热题链表相关题目-第二次做

--- src/LeetCode/LianBiao/LeetCode_141.java
+++ src/LeetCode/LianBiao/LeetCode_141.java
@@ -2,8 +2,6 @@ package LeetCode.LianBiao;
 
 import LeetCode.LianBiao.ListNode.ListNode;
 
-import java.util.List;
-
 // 灵神：快慢指针，简洁代码
 // https://leetcode.cn/problems/linked-list-cycle/solutions/1999269/mei-xiang-ming-bai-yi-ge-shi-pin-jiang-t-c4sw/?envType=study-plan-v2&envId=top-100-liked
 
@@ -12,22 +10,43 @@ import java.util.List;
 
 // 重点关注
 // 141. 环形链表
-public class LeetCode_1471 {
+public class LeetCode_141 {
     public static void main(String[] args) {
 
     }
 
-    // 再写，增添条件：考虑fast != null
+    // 第二次解出，但是思考太久，代码太冗余
+    /*
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+        if(head == null) return false;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode slow = dummy, fast = head;
+        while(fast != null && fast.next != null){
+            if(slow != fast){
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
             if(slow == fast){
                 return true;
             }
         }
         return false;
+    }
+     */
+
+    // 再写，增添条件：考虑fast != null
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head; // 乌龟和兔子同时从起点出发
+        // 检测当前fast是否为空，因为fast.next不为空时，fast.next.next可能为null，一旦fast = null 则不能后移，即结束。
+        while (fast != null && fast.next != null) { // 终止条件
+            slow = slow.next; // 乌龟走一步
+            fast = fast.next.next; // 兔子走两步
+            if (fast == slow) // 兔子追上乌龟（套圈），说明有环
+                return true;
+        }
+        return false; // 访问到了链表末尾，无环
     }
 
     // 第一次未解出，需要考虑fast == null的情况
