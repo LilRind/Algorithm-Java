be683ec4d10b6a0a68f1dc5432ca83d15786557d
LilRind <1337476737@qq.com>
2024-06-19 16:47:29 +0800

    LeetCode热题哈希相关题目-第2次解出

--- src/LeetCode/Hash/LeetCode_1.java
+++ src/LeetCode/Hash/LeetCode_1.java
@@ -15,8 +15,7 @@ public class LeetCode_1 {
         System.out.println(Arrays.toString(twoSum(c, 6)));
 
     }
-
-    // 第一次解出，注意map的用法名称
+    // 第2次解出
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -27,6 +26,18 @@ public class LeetCode_1 {
         }
         return new int[]{};
     }
+
+    // 第一次解出，注意map的用法名称
+//    public static int[] twoSum(int[] nums, int target) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if(map.containsKey(target - nums[i])){
+//                return new int[]{map.get(target - nums[i]), i};
+//            }
+//            map.put(nums[i], i);
+//        }
+//        return new int[]{};
+//    }
 
     // 之前写的
     /*
