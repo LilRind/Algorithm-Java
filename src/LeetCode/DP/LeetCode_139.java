e2cdbdb0b57aea12c1cc356f9c2b4fe1f8401515
LilRind <1337476737@qq.com>
2024-07-18 11:24:17 +0800

    LeetCode热题动态规划相关题目-第2次未解出

--- src/LeetCode/DP/LeetCode_139.java
+++ src/LeetCode/DP/LeetCode_139.java
@@ -39,26 +39,51 @@ public class LeetCode_139 {
         System.out.println(wordBreak(s3, dict3));
     }
 
-    // 提交代码示例：掌握。
+    // 2
+    /*
+     核心思想：定义 dp[n] = true 代表前n位(包括第n位)可以被wordDict拼成。
+     要求出第n位能不能拼出，可以得到转移方程：dp[n] = dp[i - len] + 第i到第i-len位等于某个字符串w。
+     核心步骤：第一步，构建外层循环i = 1到i = n，第二步遍历wordDict中的每个字符串w，判断条件是否符合，符合就置dp[i] = true
+     */
     public static boolean wordBreak(String s, List<String> wordDict) {
-        // 更改后
         int n = s.length();
-        // dp[i] 表示 s 的前 i 个字符是否可以由字典中的单词拼接成
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-
-        for (int i = 1; i <= n; i++) {
-            for (String word : wordDict) {
-                int len = word.length();
-                if (i >= len && dp[i - len] && s.substring(i - len, i).equals(word)) {
-                    dp[i] = true;
-                    break; // 一旦找到了一个匹配的单词，立即跳出循环
+        // 定义dp[n] = true 代表前n位(包括第n位)可以被wordDict拼成
+        boolean[] dp = new boolean[n + 1]; // 一般都是构建 n+1，从1开始dp方便计算
+        dp[0] = true; // 第0位默认可以拼成
+        for(int i = 1; i <= n; i++){ // 从第1位开始
+            for(String w : wordDict){ // 遍历每个wordDict的字符串 w
+                int len = w.length(); // 先求出当前字符串的长度 len
+                // i >= len 可以跳过不必要的情况，并且后续条件 dp[i - len] 要求下标不能越界。
+                // 如果 dp[i] = true 要成立，需满足dp[i - len] = true和 第i到第i-len位等于某个字符串w。
+                if(i >= len && dp[i - len] && s.substring(i - len, i).equals(w)){
+                    dp[i] = true; // 第i位前可以拼出
+                    break; // 第i位前可以拼出，跳出循环
                 }
             }
         }
-
-        return dp[n];
+        return dp[n]; // 对字符串s中的每1位都尝试检测后，返回最后结果
     }
+
+    // 1
+    // 提交代码示例：掌握。
+//    public static boolean wordBreak(String s, List<String> wordDict) {
+//        // 更改后
+//        int n = s.length();
+//        // dp[i] 表示 s 的前 i 个字符是否可以由字典中的单词拼接成
+//        boolean[] dp = new boolean[n + 1];
+//        dp[0] = true;
+//
+//        for (int i = 1; i <= n; i++) {
+//            for (String word : wordDict) {
+//                int len = word.length();
+//                if (i >= len && dp[i - len] && s.substring(i - len, i).equals(word)) {
+//                    dp[i] = true;
+//                    break; // 一旦找到了一个匹配的单词，立即跳出循环
+//                }
+//            }
+//        }
+//        return dp[n];
+//    }
 
     // 官方：dp。比较慢
     /*
