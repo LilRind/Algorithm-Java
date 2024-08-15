248d313aa0e805902eaa10d0cdd4ffe31d396fe4
LilRind <1337476737@qq.com>
2024-08-15 10:28:02 +0800

    CodeTop热题中相关题目

--- src/CodeTop/LeetCode_102.java
+++ src/CodeTop/LeetCode_102.java
@@ -41,9 +41,51 @@ public class LeetCode_102 {
         System.out.println(levelOrder(c));
     }
 
+    // 3
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null) return List.of();
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n = q.size();
+            List<Integer> level = new ArrayList<>();
+            while(n-- > 0){
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+
+    // 2
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null) return List.of();
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n = q.size();
+            List<Integer> level = new ArrayList(n);
+            while(n-- > 0){
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
     // 1
 
     // 灵神：一个队列
+    /*
     public static List<List<Integer>> levelOrder(TreeNode root) {
         if (root == null) return List.of(); // 根节点为 null，直接返回
         List<List<Integer>> ans = new ArrayList<>(); // 创建 最终 List
@@ -62,6 +104,7 @@ public class LeetCode_102 {
         }
         return ans;
     }
+     */
 
     // 灵神：两个数组
     /*
--- src/CodeTop/LeetCode_88.java
+++ src/CodeTop/LeetCode_88.java
@@ -20,7 +20,10 @@ public class LeetCode_88 {
         }
     }
 
+    // 3
 
+    // 2
+    /*
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m - 1, p2 = n - 1, p = m + n - 1;
         while(p2 >= 0){
@@ -31,21 +34,35 @@ public class LeetCode_88 {
             }
         }
     }
+    */
 
+    // 灵神：逆序双指针
+    // 先获得 nums1的有值长度指针 p1、全部长度指针 p、nums2的长度 p2，接着循环条件是 nums2 还有要合并的元素（p2 >= 0）
+    // 那么考虑什么条件 p 的位置 放入 p1 的元素（p1 还有元素且 p1 的值 > p2 的值），else下是再放入 p2 的元素。循环结束则合并结束
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // p1 指向
+        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+        while (p2 >= 0) { // nums2 还有要合并的元素
+            // 如果 p1 < 0，那么走 else 分支，把 nums2 合并到 nums1 中
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p--] = nums1[p1--]; // 填入 nums1[p1]
+            } else {
+                nums1[p--] = nums2[p2--]; // 填入 nums2[p1]
+            }
+        }
+    }
 
+    // 改进：基于灵神的逆序双指针，判断改为 nums2[p2] >= nums1[p1]
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+        while (p2 >= 0) { // nums2 还有要合并的元素
+            if (p1 < 0 || nums2[p2] >= nums1[p1]) { // 如果nums1已经全部放入数组 或者 nums2的当前元素 >= nums1的当前元素
+                nums1[p--] = nums2[p2--];
+            } else { // nums1 还有元素，而且 nums1 的当前元素 > nums2的当前元素
+                nums1[p--] = nums1[p1--];
+            }
+        }
+    }
 
-
-//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-//        // p1 指向
-//        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
-//        while (p2 >= 0) { // nums2 还有要合并的元素
-//            // 如果 p1 < 0，那么走 else 分支，把 nums2 合并到 nums1 中
-//            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-//                nums1[p--] = nums1[p1--]; // 填入 nums1[p1]
-//            } else {
-//                nums1[p--] = nums2[p2--]; // 填入 nums2[p1]
-//            }
-//        }
-//    }
 
 }
